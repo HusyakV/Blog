8c98b7a89d74f3e9d831018043534d2250b5fe4e
Valik <husyakvvv@gmail.com>
2022-02-06 20:55:24 +0200

    Create db and table for blog

--- index.php
+++ index.php
@@ -6,36 +6,24 @@ include_once ('post.php');
 <div class="container">
     <div class="row">
         <div class="col-md-8">
-            <?php for ($i=0;$i<10;$i++) {?>
-            <div class="media">
-                <div class="media-left media-top">
-                    <img alt="" src="images/girl.jpg" class="media-object" style="width: 150px">
-                    <p>
-                        Author: Admin<br>
-                        Created: 2020-11-10
-                    </p>
+            <?php for ($i=0;$i<10;$i++) { ?>
+                <div style = "display: flex;">
+                    <div class="media-left media-top" style="margin-top: 10px">
+                        <img alt="" src="images/girl.jpg" class="media-object" style="width: 150px">
+                        <p>
+                            Author: Admin<br>
+                            Created: 2020-11-10
+                        </p>
+                    </div>
+                    <div style = "margin-left: 10px; margin-top: 10px" >
+                        <h4 class="media-heading"><a href="">Title </a></h4>
+                        sdfsd sdfsdf sdfsdf sdfsdf sdfsdf sdfsdfsdf sdfsdf sdfsdf sdfsd sdfsd sdfsdf
+                        sdfsdfsdf sdfsdf sdfsdf sdfsdfs sdfsdf sdfsdf sdfsdfsd dsfsdf sdfsdf sdfsdff
+                        sdfsdfsdf sdfsdf sdfsdf sdfsdfs sdfsdf sdfsdf sdfsdfsd dsfsdf sdfsdf sdfsdff
+                        sdfsdfsdf sdfsdf sdfsdf sdfsdfs sdfsdf sdfsdf sdfsdfsd dsfsdf sdfsdf sdfsdff
+                    </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading"><a href="">Title </a></h4>
-                    sdfsd sdfsdf sdfsdf sdfsdf sdfsdf sdfsdfsdf sdfsdf sdfsdf sdfsd sdfsd sdfsdf
-                    sdfsdfsdf sdfsdf sdfsdf sdfsdfs sdfsdf sdfsdf sdfsdfsd dsfsdf sdfsdf sdfsdff
-                    sdfsdfsdf sdfsdf sdfsdf sdfsdfs sdfsdf sdfsdf sdfsdfsd dsfsdf sdfsdf sdfsdff
-                    sdfsdfsdf sdfsdf sdfsdf sdfsdfs sdfsdf sdfsdf sdfsdfsd dsfsdf sdfsdf sdfsdff
-                </div>
-
-            </div>
             <?php } ?>
         </div>
     </div>
-
 </div>
-<style>
-    }
-    body{
-        text-align: justify;
-    }
-    .media-left{
-        float: left;
-        margin: 0px 0px 50px 50px;
-    }
-</style>
